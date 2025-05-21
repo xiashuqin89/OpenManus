4056c1b2b58b1d4d674dc5566233e7e55e6f4ab0
neolei <neolei@tencent.com>
2025-05-21 22:21:28 +0800

    add exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -11,3 +11,34 @@ class OpenManusError(Exception):
 
 class TokenLimitExceeded(OpenManusError):
     """Exception raised when the token limit is exceeded"""
+
+
+class ApiError(Exception, RuntimeError):
+    pass
+
+
+class HttpFailed(ApiError):
+    """HTTP status code is not 2xx."""
+
+    def __init__(self, status_code):
+        self.status_code = status_code
+
+
+class ActionFailed(ApiError):
+    """
+    Action failed to execute.
+
+    >>> except ActionFailed as e:
+    >>>     if e.code > 0:
+    >>>         pass  # error code returned by HTTP API
+    >>>     elif e.code < 0:
+    >>>         pass  # error code returned by CoolQ
+    """
+
+    def __init__(self, code, info=None):
+        self.code = code
+        self.info = info
+
+
+class NetworkError(Exception, IOError):
+    pass
